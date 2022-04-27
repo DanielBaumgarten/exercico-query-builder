d784c48d23c30e264947d92662bea069c7044cc1
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-04-27 17:47:54 -0300

    ajuste de rotas

--- src/rotas.js
+++ src/rotas.js
@@ -3,5 +3,10 @@ const usuarios = require('./controladores/usuarios');
 
 const rotas = express();
 
+rotas.get('/usuarios', usuarios.listar);
+rotas.get('/usuarios/:id', usuarios.obter);
+rotas.post('/usuarios', usuarios.cadastrar);
+rotas.put('/usuarios/:id',usuarios.atualizar);
+rotas.delete('/usuarios/:id', usuarios.excluir);
 
 module.exports = rotas;
